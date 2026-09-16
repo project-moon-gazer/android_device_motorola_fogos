616cb613aba5599d372a97493aec9dae89d8e90b
miss_rais <miss_rais@proton.me>
2026-09-16 10:19:57 +0000

    added config for dolby

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 # Inherit from motorola sm6375-common
 $(call inherit-product, device/motorola/sm6375-common/common.mk)
 
+# Dolby
+$(call inherit-product, vendor/lunaris/dolby/dolby.mk)
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResDevice \
